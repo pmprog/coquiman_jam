d38bc81958915b8e009419ee78c8eefbdc448d07
Nikku4211 <73254369+nikku4211@users.noreply.github.com>
2023-02-05 17:40:02 -0500

    Added graphics

---
+++ resources/flatbuilding.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="flatbuilding" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="../graphdraft/flatbuildingtiles.png" width="256" height="256"/>
+</tileset>
