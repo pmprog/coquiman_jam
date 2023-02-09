0675d114deaf401f065cc750eb68cd46ff25d458
pmprog <devhead@pmprog.co.uk>
2023-02-09 22:15:44 +0000

    New intro transition?

--- src/boot.c
+++ src/boot.c
@@ -11,6 +11,8 @@ static u16 x = 0;
 static u16 y = 0;
 static u16 c = 1;
 
+static s8 drawy[30] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+
 StageRecord bootstage = { bootstage_start, bootstage_update, bootstage_finish };
 
 void bootstage_start()
@@ -36,12 +38,28 @@ void bootstage_finish()
 
 void bootstage_update()
 {
+    if(kit_stage_time % 6 != 0) {return;}
 
-//    if(kit_stage_time % 2 == 0)
-//    {
-        tilemap_map_settile( 0, 0, x, y, map_title_gfx_titlebkg_map_data[ (y * map_title_gfx_titlebkg_map_width) + x ] );
-        if( c < 510) {c++;}
-        x = (x + 1) % 30;
-        if(x == 0) { y++;}
-//    }
+    u8 completed = 0;
+
+    for(u8 x = 0; x < 30; x++)
+    {
+        if(drawy[x] < 20)
+        {
+            u8 targety = drawy[x] + ((random_next() & 0x0001) + 1);
+            for(s8 y = drawy[x]; y <= targety; y++ )
+            {
+                if( y >= 0 && y < 20 )
+                {
+                    tilemap_map_settile( 0, 0, x, y, map_title_gfx_titlebkg_map_data[ (y * map_title_gfx_titlebkg_map_width) + x ] );
+                }
+            }
+            drawy[x] = targety;
+        }
+        else
+        {
+            completed++;
+        }
+    }
+
 }
